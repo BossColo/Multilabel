51e70b8059b338327ef288bea26ff5c9387d86b9
Ross Cohen <rjc55@njit.edu>
2016-04-26 15:35:37 -0400

    0.2.2 - Don't require matplotlib, it breaks Anaconda

--- setup.py
+++ setup.py
@@ -2,13 +2,12 @@ from distutils.core import setup
 setup(
   name = 'multilabel',
   py_modules = ['multilabel'],
-  version = '0.2.1',
+  version = '0.2.2',
   description = 'A Python module that will create multi-line xlabels in matplotlib with left-side titles',
   author = 'Ross Cohen',
   author_email = 'rjc55@njit.edu',
   url = 'https://github.com/BossColo/multilabel',
-  download_url = 'https://github.com/BossColo/multilabel/tarball/0.2.1',
+  download_url = 'https://github.com/BossColo/multilabel/tarball/0.2.2',
   keywords = ['Python', 'matplotlib', 'xlabels'],
-  install_requires = ['matplotlib'],
   classifiers = [],
 )
