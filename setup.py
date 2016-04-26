301c4c753968609d42e64ff8b06fce7ba63628b9
Ross Cohen <rjc55@njit.edu>
2016-04-26 15:01:15 -0400

    Gonna get it right one of these times

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'multilabel',
   py_modules = ['multilabel'],
-  version = '0.2',
+  version = '0.2.1',
   description = 'A Python module that will create multi-line xlabels in matplotlib with left-side titles',
   author = 'Ross Cohen',
   author_email = 'rjc55@njit.edu',
   url = 'https://github.com/BossColo/multilabel',
-  download_url = 'https://github.com/BossColo/multilabel/tarball/0.2',
+  download_url = 'https://github.com/BossColo/multilabel/tarball/0.2.1',
   keywords = ['Python', 'matplotlib', 'xlabels'],
-  install_requires = ['bisect', 'matplotlib', 'datetime'],
+  install_requires = ['matplotlib'],
   classifiers = [],
 )
